f8443345714eb350b9d6f4eb75f730704d2846d3
nicholaicascio <lordcohliani@gmail.com>
2019-09-15 23:37:21 -0600

    Basic move controller
    this is going to be a top down 2.5D game. I am making a custom move controller. Basics are done right now, next up is a placeholder state machine for sprinting or sneaking probably.

---
+++ Sword of the Exiled/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
+    Vector3 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
