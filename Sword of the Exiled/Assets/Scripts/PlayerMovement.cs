b046962e3ae5d553035a3137307b75fadeef7dce
nicholaicascio <lordcohliani@gmail.com>
2019-09-16 18:14:30 -0600

    sprinting and sneaking
    continuing work on character controller

--- Sword of the Exiled/Assets/Scripts/PlayerMovement.cs
+++ Sword of the Exiled/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 3f;
+    public float sprintSpeed = 8f;
+    public float sneakSpeed = 2.5f;
     public Rigidbody rb;
     Vector3 movement;
+
+    bool sprinting;
+    bool sneaking;
 
 
     // Start is called before the first frame update
@@ -14,18 +19,43 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
+        sprinting = Input.GetButton("Sprint");
+
+        if (sneaking == false && Input.GetButtonDown("Sneak") == true)
+        {
+            sneaking = true;
+        }
+        else if (sneaking == true && Input.GetButtonDown("Sneak") == true)
+        {
+            sneaking = false;
+        }
+        else if (sneaking == true && Input.GetButtonDown("Sprint") == true)
+        {
+            sneaking = false;
+        }
     }
 
     private void FixedUpdate()
     {
         //Movement
+        if (sprinting == true)
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else if (sneaking == true)
+        {
+            moveSpeed = sneakSpeed;
+        }
+        else
+        {
+            moveSpeed = 5f;
+        }
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
