e48bd66a76525b1b42815230d53720b81418efc1
nicholaicascio <lordcohliani@gmail.com>
2019-09-26 17:42:58 -0600

    setting up raycast for lasers
    I want puzzles where you bend the lasers with prisms. so i am setting up raycasts to come from the lasers to detect prisms. tthat is set up now, just need to make the prism shoot a new laser to simulate bending the light

---
+++ Sword of the Exiled/Assets/Scripts/LaserRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRaycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "UpPrism")
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+                Debug.Log(hit.collider.gameObject.name);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+                //Debug.Log("Did not Hit");
+            }
+        }
+
+
+    }
+}
