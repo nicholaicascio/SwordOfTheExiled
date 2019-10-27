ddd50101a45d4f829838926f48fd04c1911a1a03
nicholaicascio <lordcohliani@gmail.com>
2019-10-26 18:47:42 -0600

    prism tooltips and grabbable objects
    prisms all have tooltips and grabbable objects now. There are some glitches, tooltips are defaulting to on and then they work as expected after the ontriggerexit is tripped the first time. I also need to make it so that you can't grab the prisms which are supposed to be stationary (eg locked in place)

---
+++ Sword of the Exiled/Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+    public Transform player;
+    //public Transform playerCam;
+    public float throwForce = 100;
+    bool hasPlayer = false;
+    bool beingCarried = false;
+    public AudioClip[] soundToPlay;
+    //private AudioSource audioSource;
+    public int dmg;
+    private bool touched = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 2.5f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+        if (hasPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = player;
+            beingCarried = true;
+        }
+        if (beingCarried)
+        {
+            if (touched)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                //GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+                RandomAudio();
+            }
+        }
+    }
+
+    void RandomAudio()
+    {
+
+    }
+}
---
+++ Sword of the Exiled/Assets/Scripts/ToolTipShower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipShower : MonoBehaviour
+{
+    public Canvas tooltip;
+    bool playerIsInside = false;
+
+    void Update()
+    {
+        if (!playerIsInside)
+        {
+            tooltip.gameObject.SetActive(false);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        tooltip.gameObject.SetActive(true);
+        playerIsInside = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        tooltip.gameObject.SetActive(false);
+        playerIsInside = false;
+    }
+}
