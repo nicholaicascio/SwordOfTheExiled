00dc3975cd27e384dfbca57ebb972e56dba2f15b
nicholaicascio <lordcohliani@gmail.com>
2019-11-02 16:52:43 -0600

    cutscene
    intro cutscene getting set up. camera dolly around to look real nice and fancy. I am going to be setting up the menu and stuff next.

---
+++ Sword of the Exiled/Assets/Scripts/MainMenuNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    public void pressPlay()
+    {
+        anim.SetBool("Play", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
