afcaaf3eef2e85caab46688286c0051b1edcc293
nicholaicascio <lordcohliani@gmail.com>
2019-11-23 17:37:41 -0700

    finished opening scene
    reflection probe makes all the difference

--- Sword of the Exiled/Assets/Scripts/MainMenuNavigation.cs
+++ Sword of the Exiled/Assets/Scripts/MainMenuNavigation.cs
@@ -6,7 +6,10 @@ public class MainMenuNavigation : MonoBehaviour
 {
     private Animator CamAnim;
     public Animator CanvAnim;
-    private Animation[] clips;
+    //private Animation[] clips;
+    public Canvas AreYouSure;
+    public GameObject LobbyCam;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +23,34 @@ public class MainMenuNavigation : MonoBehaviour
         CamAnim.SetBool("Play", true);
         CanvAnim.SetBool("playPressed", true);
     }
+    public void pressLobby()
+    {
+        Camera thiscam = this.gameObject.GetComponent<Camera>();
+        thiscam.enabled = false;
+        LobbyCam.gameObject.SetActive(true);
+    }
 
     public void DollyEnded()
     {
         //Debug.Log("dollycomplete");
         CanvAnim.SetBool("dollyComplete", true);
     }
+
+    public void ExitClicked()
+    {
+        AreYouSure.gameObject.SetActive(true);
+    }
+    
+    public void ExitCancelled()
+    {
+        AreYouSure.gameObject.SetActive(false);
+    }
+
+    public void ExitConfirmed()
+    {
+        Application.Quit();
+    }
+
     // Update is called once per frame
     void Update()
     {
