a95a638d76b0af44572b619fa74bbeb4ab0e3263
nicholaicascio <lordcohliani@gmail.com>
2019-12-03 10:00:40 -0700

    branches are conbined
    trying to fix colliders. there's one which opens a door in room 4 and one which i just added that should take us to the credits.

---
+++ Sword of the Exiled/Assets/Scripts/TransitionToCredits.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+
+public class TransitionToCredits : MonoBehaviour
+{
+    private const byte TRANS_TO_CREDITS = 0;
+
+    private void OnEnable()
+    {
+        Debug.Log("Adding listener for transition to credits");
+        PhotonNetwork.NetworkingClient.EventReceived += NetworkingClient_EventReceived;
+    }
+
+    private void NetworkingClient_EventReceived(EventData obj)
+    {
+        //First, check for our event.
+        if (obj.Code == TRANS_TO_CREDITS)
+        {
+            //This is it.  We'll go ahead and transition to credits
+            Debug.Log("collide");
+            SceneManager.LoadScene("CreditsScene");
+
+            //Now that we have opened the door, remove the listener so this doesn't happen again.
+            RemoveEvent_TransToCredits();
+        }
+    }
+
+    private void RemoveEvent_TransToCredits()
+    {
+        Debug.Log("Removing listener for door opening.");
+        PhotonNetwork.NetworkingClient.EventReceived -= NetworkingClient_EventReceived;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Now, we're sending an event.
+        PhotonNetwork.RaiseEvent(TRANS_TO_CREDITS, null, RaiseEventOptions.Default, SendOptions.SendUnreliable);
+    }
+}
