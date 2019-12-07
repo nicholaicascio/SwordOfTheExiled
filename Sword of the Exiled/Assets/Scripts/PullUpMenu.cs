014c51751e5749ba7a880ccfb0cd70ce18362372
nicholaicascio <lordcohliani@gmail.com>
2019-12-07 16:16:35 -0700

    gameplay music and quit menu
    There is now music which works in the gameplay scene for both players. Also players can now press escape and a menu will pull up to allow them to quit the game.

---
+++ Sword of the Exiled/Assets/Scripts/PullUpMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PullUpMenu : MonoBehaviour
+{
+    public GameObject quitCanv;
+    public bool esc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //quitCanv = GameObject.FindGameObjectWithTag("QuitGameCanvas");
+        quitCanv = GameObject.Find("QuitGameCanvas");
+        quitCanv.gameObject.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        esc = Input.GetButton("Menu");
+        if (esc)
+        {
+            quitCanv.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Sword of the Exiled/Assets/Scripts/QuitGameCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameCanvas : MonoBehaviour
+{
+    public void YesPressed()
+    {
+        Application.Quit();
+    }
+
+    public void NoPressed()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
